827c789741a885249f9583df032ea5de5f23d6ee
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-03 12:29:01 -0400

    refactor: Modified Mapper's Traverse method parameters

--- types/rest/map.go
+++ types/rest/map.go
@@ -14,23 +14,21 @@ type (
 
 	// Map struct for the REST endpoints
 	Map struct {
-		Debug         bool
 		Interceptions *map[string]map[Method]grpc.Method
 		ChildrenMaps  *map[string]*Map
 	}
 )
 
 // NewMap creates a new REST map
-func NewMap(debug bool, interceptions *map[string]map[Method]grpc.Method, childrenMaps *map[string]*Map) *Map {
+func NewMap(interceptions *map[string]map[Method]grpc.Method, childrenMaps *map[string]*Map) *Map {
 	return &Map{
-		Debug:         debug,
 		Interceptions: interceptions,
 		ChildrenMaps:  childrenMaps,
 	}
 }
 
 // Traverse traverses the REST map
-func (m *Map) Traverse(relativeURI string, restMethod Method) (*grpc.Method, error) {
+func (m *Map) Traverse(debug bool, relativeURI string, restMethod Method) (*grpc.Method, error) {
 	var firstHalfUri, secondHalfUri string
 
 	// Get the first half of the relative URI
@@ -49,8 +47,9 @@ func (m *Map) Traverse(relativeURI string, restMethod Method) (*grpc.Method, err
 		secondHalfUri = relativeURI[firstHalfUriIndex:]
 	}
 
-	if m.Debug {
-		// Print if the URI is an endpoint. first half URI and second half URI if debug is enabled
+	// Check if debug is enabled
+	if debug {
+		// Print if the URI is an endpoint, first half URI and second half URI
 		fmt.Printf("Is endpoint: %t\n", isEndpoint)
 		fmt.Printf("First half URI: %s\n", firstHalfUri)
 		fmt.Printf("Second half URI: %s\n", secondHalfUri)
@@ -76,7 +75,7 @@ func (m *Map) Traverse(relativeURI string, restMethod Method) (*grpc.Method, err
 	if m.ChildrenMaps != nil {
 		// Check if the first half URI is in the children maps. If it is, traverse the child map
 		if childMap, ok := (*m.ChildrenMaps)[firstHalfUri]; ok {
-			return childMap.Traverse(secondHalfUri, restMethod)
+			return childMap.Traverse(debug, secondHalfUri, restMethod)
 		}
 	} else {
 		return nil, fmt.Errorf(MissingChildrenMap, relativeURI)
