ce2e992c42f38141e8f21a47254fd36d7cb44365
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-03 16:33:23 -0400

    refactor: I think this finally works...

--- types/rest/map.go
+++ types/rest/map.go
@@ -29,56 +29,57 @@ func NewMap(interceptions *map[string]map[Method]grpc.Method, childrenMaps *map[
 
 // Traverse traverses the REST map
 func (m *Map) Traverse(debug bool, relativeURI string, restMethod Method) (*grpc.Method, error) {
-	var firstHalfUri, secondHalfUri string
+	var firstPartUri, secondPartUri string
 
-	// Get the first half of the relative URI
-	firstHalfUriIndex := -1
-	if len(relativeURI) > 1 {
-		firstHalfUriIndex = strings.Index(relativeURI[1:], "/") + 1
+	// Get the second part index of the relative URI
+	secondPartUriIndex := strings.Index(relativeURI[1:], "/")
+	if secondPartUriIndex != -1 {
+		secondPartUriIndex++
 	}
 
-	// Check if the URI is the endpoint or a URI with a parameter
+	// Check if the URI is an endpoint or a URI with a parameter
 	isEndpoint := false
-	if firstHalfUriIndex == -1 || len(relativeURI) == firstHalfUriIndex+1 || relativeURI[firstHalfUriIndex+1] == ':' {
-		firstHalfUri = relativeURI
+	if secondPartUriIndex == -1 {
+		firstPartUri = "/"
+		isEndpoint = true
+	} else if relativeURI[secondPartUriIndex+1] == ':' {
+		firstPartUri = relativeURI[secondPartUriIndex:]
 		isEndpoint = true
 	} else {
-		firstHalfUri = relativeURI[:firstHalfUriIndex]
-		secondHalfUri = relativeURI[firstHalfUriIndex:]
+		firstPartUri = relativeURI[:secondPartUriIndex]
+		secondPartUri = relativeURI[secondPartUriIndex:]
 	}
 
 	// Check if debug is enabled
 	if debug {
 		// Print if the URI is an endpoint, first half URI and second half URI
 		fmt.Printf("Is endpoint: %t\n", isEndpoint)
-		fmt.Printf("First half URI: %s\n", firstHalfUri)
-		fmt.Printf("Second half URI: %s\n", secondHalfUri)
+		fmt.Printf("First half URI: %s\n", firstPartUri)
+		fmt.Printf("Second half URI: %s\n", secondPartUri)
 	}
 
-	// Check if the first half URI is in the interceptions map
-	if m.Interceptions != nil {
-		if methodMap, ok := (*m.Interceptions)[firstHalfUri]; ok {
+	// Check if the first part of the URI is in the interceptions map
+	if m.Interceptions != nil && isEndpoint {
+		if methodMap, ok := (*m.Interceptions)[firstPartUri]; ok {
 			if grpcMethod, ok := methodMap[restMethod]; ok {
 				return &grpcMethod, nil
 			}
 		}
-
-		// Check if the URI is an endpoint
-		if isEndpoint {
-			return nil, fmt.Errorf(MissingEndpointInterception, relativeURI, restMethod)
-		}
-	} else if isEndpoint {
-		return nil, fmt.Errorf(MissingInterceptions, relativeURI, restMethod)
 	}
 
-	// Check if the first half URI is in the children maps
+	// Check if the URI is an endpoint
+	if isEndpoint {
+		return nil, fmt.Errorf(MissingEndpointInterception, relativeURI, restMethod)
+	}
+
 	if m.ChildrenMaps != nil {
-		// Check if the first half URI is in the children maps. If it is, traverse the child map
-		if childMap, ok := (*m.ChildrenMaps)[firstHalfUri]; ok {
-			return childMap.Traverse(debug, secondHalfUri, restMethod)
+		// Check if the first part of the URI is in the children maps
+		// If it is, traverse the child map
+		if childMap, ok := (*m.ChildrenMaps)[firstPartUri]; ok {
+			return childMap.Traverse(debug, secondPartUri, restMethod)
 		}
-	} else {
-		return nil, fmt.Errorf(MissingChildrenMap, relativeURI, restMethod)
+		return nil, fmt.Errorf(FailedToTraverse, relativeURI, restMethod)
 	}
-	return nil, fmt.Errorf(FailedToTraverse, relativeURI, restMethod)
+
+	return nil, fmt.Errorf(MissingChildrenMap, relativeURI, restMethod)
 }
