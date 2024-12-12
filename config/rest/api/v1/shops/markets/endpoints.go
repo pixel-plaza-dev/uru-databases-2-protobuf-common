da1721a431125f62093d3715fb25af8b215075e1
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-11 22:01:43 -0400

    fix: Added missing shops markets base endpoint

---
+++ config/rest/api/v1/shops/markets/endpoints.go
@@ -0,0 +1,8 @@
+package markets
+
+import (
+	typesrest "github.com/pixel-plaza-dev/uru-databases-2-protobuf-common/types/rest"
+)
+
+// Base is the base endpoint for the shop service markets REST endpoints
+var Base = typesrest.NewBaseEndpoint("markets")
